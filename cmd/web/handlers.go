013a1ea993a14dfffa30fcae8a71691749665df2
muchiri08 <mbogokennedy08@gmail.com>
2023-10-25 21:26:11 +0300

    signin/signup design done

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -7,3 +7,11 @@ import (
 func (app *application) index(w http.ResponseWriter, r *http.Request) {
 	app.render(w, r, "index.page.html", nil)
 }
+
+func (app *application) signupForm(w http.ResponseWriter, r *http.Request) {
+	app.render(w, r, "signup.page.html", nil)
+}
+
+func (app *application) signinForm(w http.ResponseWriter, r *http.Request) {
+	app.render(w, r, "signin.page.html", nil)
+}
--- cmd/web/router.go
+++ cmd/web/router.go
@@ -10,6 +10,8 @@ func (app *application) routes() *httprouter.Router {
 	router := httprouter.New()
 
 	router.HandlerFunc(http.MethodGet, "/", app.index)
+	router.HandlerFunc(http.MethodGet, "/signup", app.signupForm)
+	router.HandlerFunc(http.MethodGet, "/signin", app.signinForm)
 
 	// serving static files
 	router.ServeFiles("/static/*filepath", http.Dir("./ui/static"))
